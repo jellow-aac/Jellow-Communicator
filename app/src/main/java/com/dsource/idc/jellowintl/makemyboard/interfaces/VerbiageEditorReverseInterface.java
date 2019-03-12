689660a2aef101e6d3358ed6742e37f6d069fbff
Rahul Jidge <rahul.jidge@gmail.com>
2019-03-12 15:09:04 +0530

    Revert "Interfaces required for jellow board integration"
    This reverts commit ac2e0f86d8a58c019fd01d3bd42626d4afceea18.

--- app/src/main/java/com/dsource/idc/jellowintl/makemyboard/interfaces/VerbiageEditorReverseInterface.java
+++
@@ -1,7 +0,0 @@
-package com.dsource.idc.jellowintl.makemyboard.interfaces;
-
-import android.graphics.Bitmap;
-
-public interface VerbiageEditorReverseInterface {
-    void onPhotoResult(Bitmap bitmap, int code, String fileName);
-}
