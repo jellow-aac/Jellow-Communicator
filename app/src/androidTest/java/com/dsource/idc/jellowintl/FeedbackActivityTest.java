ee73a6a134c03bbde6e466f6c0811e11dec5a1d2
Rahul Jidge <rahul.jidge@gmail.com>
2019-06-04 18:40:18 +0530

    Updates: - Corrected failing test cases. - Updated dependencies in build.gradle

--- app/src/androidTest/java/com/dsource/idc/jellowintl/FeedbackActivityTest.java
+++ app/src/androidTest/java/com/dsource/idc/jellowintl/FeedbackActivityTest.java
@@ -10,6 +10,8 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
@@ -29,10 +31,10 @@ public class FeedbackActivityTest {
 
     @Test
     public void validateRatedEasyToUse(){
-        onView(withId(R.id.pictures)).perform(click());
-        onView(withId(R.id.voice)).perform(click());
+        onView(withId(R.id.pictures)).perform(click(), swipeUp());
+        onView(withId(R.id.voice)).perform(click(), swipeUp());
         onView(withId(R.id.navigate)).perform(click());
-        onView(withId(R.id.comments)).perform(typeText("Awesome app"));
+        onView(withId(R.id.comments)).perform(typeText("Awesome app"), closeSoftKeyboard());
         onView(withId(R.id.bSubmit)).perform(click());
         onView(withText(R.string.rate_jellow))
             .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
@@ -41,10 +43,10 @@ public class FeedbackActivityTest {
 
     @Test
     public void validateRatedClearPictures(){
-        onView(withId(R.id.easy_to_use)).perform(click());
-        onView(withId(R.id.voice)).perform(click());
-        onView(withId(R.id.navigate)).perform(click());
-        onView(withId(R.id.comments)).perform(typeText("Awesome app"));
+        onView(withId(R.id.easy_to_use)).perform(click(), swipeUp());
+        onView(withId(R.id.voice)).perform(click(), swipeUp());
+        onView(withId(R.id.navigate)).perform(click(), swipeUp());
+        onView(withId(R.id.comments)).perform(typeText("Awesome app"), closeSoftKeyboard());
         onView(withId(R.id.bSubmit)).perform(click());
         onView(withText(R.string.rate_jellow))
             .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
@@ -53,10 +55,10 @@ public class FeedbackActivityTest {
 
     @Test
     public void validateRatedClearVoice(){
-        onView(withId(R.id.easy_to_use)).perform(click());
-        onView(withId(R.id.pictures)).perform(click());
-        onView(withId(R.id.navigate)).perform(click());
-        onView(withId(R.id.comments)).perform(typeText("Awesome app"));
+        onView(withId(R.id.easy_to_use)).perform(click(), swipeUp());
+        onView(withId(R.id.pictures)).perform(click(), swipeUp());
+        onView(withId(R.id.navigate)).perform(click(), swipeUp());
+        onView(withId(R.id.comments)).perform(typeText("Awesome app"), closeSoftKeyboard());
         onView(withId(R.id.bSubmit)).perform(click());
         onView(withText(R.string.rate_jellow))
             .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
@@ -65,10 +67,10 @@ public class FeedbackActivityTest {
 
     @Test
     public void validateRatedEasyToNavigate(){
-        onView(withId(R.id.easy_to_use)).perform(click());
-        onView(withId(R.id.pictures)).perform(click());
-        onView(withId(R.id.voice)).perform(click());
-        onView(withId(R.id.comments)).perform(typeText("Awesome app"));
+        onView(withId(R.id.easy_to_use)).perform(click(), swipeUp());
+        onView(withId(R.id.pictures)).perform(click(), swipeUp());
+        onView(withId(R.id.voice)).perform(click(), swipeUp());
+        onView(withId(R.id.comments)).perform(typeText("Awesome app"), closeSoftKeyboard());
         onView(withId(R.id.bSubmit)).perform(click());
         onView(withText(R.string.rate_jellow))
             .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
@@ -77,11 +79,11 @@ public class FeedbackActivityTest {
 
     @Test
     public void validateAddedComments(){
-        onView(withId(R.id.easy_to_use)).perform(click());
-        onView(withId(R.id.pictures)).perform(click());
-        onView(withId(R.id.voice)).perform(click());
-        onView(withId(R.id.navigate)).perform(click());
-        onView(withId(R.id.bSubmit)).perform(click());
+        onView(withId(R.id.easy_to_use)).perform(click(), swipeUp());
+        onView(withId(R.id.pictures)).perform(click(), swipeUp());
+        onView(withId(R.id.voice)).perform(click(), swipeUp());
+        onView(withId(R.id.navigate)).perform(click(), swipeUp());
+        onView(withId(R.id.bSubmit)).perform(click(), swipeUp());
         onView(withText(R.string.rate_jellow))
             .inRoot(withDecorView(not(is(activityRule.getActivity().getWindow().getDecorView()))))
                 .check(matches(isDisplayed()));
