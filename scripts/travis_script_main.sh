1c9c3fa3ba3034f8ee3cf2292f3ec148fb9f4f23
Rahul Jidge <rahul.jidge@gmail.com>
2019-06-30 14:52:05 +0530

    Corrected script files 2

--- scripts/travis_script_main.sh
+++ scripts/travis_script_main.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
+
 set -e
-./scripts/"$RUN_SCRIPT"
+
+if [[ "$RUN_SCRIPT" == "test_script_one.sh" ]]; then
+    ./scripts/test_script_one.sh
+elif [[ "$RUN_SCRIPT" == "test_script_two.sh" ]]; then
+    ./scripts/test_script_two.sh
+elif [[ "$RUN_SCRIPT" == "test_script_three.sh" ]]; then
+    ./scripts/test_script_three.sh
+elif [[ "$RUN_SCRIPT" == "test_script_four.sh" ]]; then
+    ./scripts/test_script_four.sh
+else
+    echo "That module doesn't exist, now does it? ;)"
+    exit 1
+fi
